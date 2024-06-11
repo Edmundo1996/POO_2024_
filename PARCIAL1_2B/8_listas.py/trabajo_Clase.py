a7fe74c6a7aef2e310de49c412afe2fb313fac32
Edmundo <mundocardoza@gmail.com>
2024-06-11 08:26:41 -0600

    Commit Final del Parcial 1 of VSCode

--- PARCIAL1_2B/8_listas.py/trabajo_Clase.py
+++ PARCIAL1_2B/8_listas.py/trabajo_Clase.py
@@ -3,20 +3,38 @@
 # utilizar funcioens y mandar llamar desde otro archivo
 #utilizar listas para almacenar los nombrs de peliculas
 
-def menu():
-    print("SELECCIONE LA OPCION SEGUN DESEA*********","OPCION ==>BORAR CONSULTAR PELICULAS 1","OPCION REMOVER PELICULAS ==> 2","OPCION AGREGAR PELICULA ==> 3",sep="\n")
+import os
+import ListaPelis
+os.system('cls')
 
-peliculas=[
-    "nemo",
-    "toy Story",
-    "Elmo",
-    "batman",
-    "Avengers",
-    "Chuky"    
-        ]
-print(peliculas)
-def opciones(consultar,borrar,agregar):
-    opcion=input("OPCION ==> ")
-    if opcion == 1:
-        peliculas
-    
+def mostrar_menu():
+    print("\n.:Gestión de Pelíclones:. ")
+    print("1. Agregar película")
+    print("2. Remover película")
+    print("3. Consultar películas")
+    print("4. Salir")
+
+def ejecutar_programa():
+    while True:
+        mostrar_menu()
+        opcion = input("Seleccione una opción: ")
+        
+        if opcion == '1':
+            nombre = input("Ingrese el nombre de la película a agregar: ")
+            ListaPelis.agregar_pelicula(nombre)
+        
+        elif opcion == '2':
+            nombre = input("Ingrese el nombre de la película a remover: ")
+            ListaPelis.remover_pelicula(nombre)
+        
+        elif opcion == '3':
+            ListaPelis.consultar_peliculas()
+        
+        elif opcion == '4':
+            print("Saliendo del programa...")
+            break
+        
+        else:
+            print("Opción no válida. Por favor, intente de nuevo.")
+
+ejecutar_programa()
